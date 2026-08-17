1ae571bf6696a664291cb334294a9b03ca1f504f
avimalliksaikat <avimalliksaikat@gmail.com>
2026-08-17 19:23:55 +0600

    Add User class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
+import model.User;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("===== MONEY MANAGEMENT =====");
-        System.out.println("Project Started!");
+
+        User user = new User(
+            1,
+            "Avi",
+            "avi123",
+            "1234"
+        );
+
+        System.out.println("ID: " + user.getId());
+        System.out.println("Name: " + user.getName());
+        System.out.println("Username: " + user.getUsername());
+        System.out.println("Password: " + user.getPassword());
     }
 }
